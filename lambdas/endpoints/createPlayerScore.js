41ebc4295712def9a53c6e53b5a123285e14e862
dantedxwolf <teacher.martin@gmail.com>
2021-01-16 02:35:25 +0600

    lambda hooks added

--- lambdas/endpoints/createPlayerScore.js
+++ lambdas/endpoints/createPlayerScore.js
@@ -1,5 +1,27 @@
 const Responses = require("../common/API_RESPONSES");
 const Dynamo = require("../common/Dynamo");
+const {withHooks} = require("../common/hooks");
+
+const tableName = process.env.tableName; // this is defined in serverless.yml
+
+const handler = async (event) =>{
+    if(!event.pathParameters.ID){
+        return Responses._400({msg:'Missing ID'});
+    }
+    let ID = event.pathParameters.ID;
+    const user = event.body;
+    user.ID = ID;
+    const newUser = await Dynamo.write(user,tableName);
+    if(!newUser){
+        return Responses._400({message:"Failed to write user by ID"});
+    }
+    return Responses._200({newUser});
+};
+
+exports.handler = withHooks(handler);
+/* Before adding hooks,
+const Responses = require("../common/API_RESPONSES");
+const Dynamo = require("../common/Dynamo");
 
 const tableName = process.env.tableName; // this is defined in serverless.yml
 
@@ -21,3 +43,4 @@ exports.handler = async (event) =>{
     }
     return Responses._200({newUser});
 };
+*/
